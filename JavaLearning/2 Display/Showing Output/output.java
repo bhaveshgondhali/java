6c2a4fe5dd334500faad106c22e749d15198376d
bhaveshgondhali <bhaveshgondhali.dev@gmail.com>
2025-06-21 22:10:47 +0530

    java and IDE setup

---
+++ JavaLearning/2 Display/Showing Output/output.java
@@ -0,0 +1,25 @@
+public class output {
+public static void main(String[] args) {
+  System.out.print("one");
+  System.out.print("Two");
+  System.out.print("Three");
+  //result will be "oneTwoThree"
+
+  System.out.println();
+  //result will be newline
+
+  System.out.println("one");
+  System.out.println("Two");
+  System.out.println("Three");
+  // result will be 
+  //   one
+  //   Two
+  //   Three 
+
+
+
+
+
+  
+}  
+}
---
+++ Programming Challenge/C1_Display_Output.java
@@ -0,0 +1,6 @@
+//Create a class to display output “good morning” check output.
+public class C1_Display_Output {
+public static void main(String[] args) {
+  System.out.println("good morning");
+}  
+}
